4ed22ca6bdf50cd88f944d11e40900d941e3c204
Vijay Mahes <Vijaypradhap2004@gmail.com>
2026-07-04 08:05:43 +0530

    Initial commit with updated developer details

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+		&copy; Copyright <?php echo date('Y'); ?>. All Rights Reserved | Developed by <a href="mailto:Vijaypradhap2004@gmail.com">Vijay Mahes</a>
+	</div>
+</div>
